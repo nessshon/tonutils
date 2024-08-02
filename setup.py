58ad02d0fd948875ab7e8735ad6e42c6403a524a
nessshon <shonness98@gmail.com>
2024-08-02 18:17:38 +0500

    Update dependencies and metadata.

--- setup.py
+++ setup.py
@@ -5,21 +5,26 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tonutils",
-    version="0.0.1",
+    version="0.0.2",
     author="nessshon",
-    description="Tonutils is a high-level OOP library for Python designed for interacting with the TON. It is built on top of three of the most popular libraries for working with TON in Python: pytoniq, pytonapi, and pytoncenter.",
+    description="Tonutils is a high-level object-oriented library for Python designed to facilitate interactions with the TON blockchain.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nessshon/tonutils/",
     packages=setuptools.find_packages(exclude=["examples"]),
     install_requires=[
-        "pytoniq~=0.1.39",
-        "pytoncenter~=0.0.14",
-        "pytonapi~=0.3.2",
+        "aiohttp~=3.9.5",
+        "pytoniq-core~=0.1.36",
     ],
+    extras_require={
+        "pytoniq": [
+            "pytoniq~=0.1.39",
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
