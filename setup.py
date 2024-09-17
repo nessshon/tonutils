15ab0bf5d4887713f47e97a217c1495b5f5cc6e2
nessshon <shonness98@gmail.com>
2024-09-18 03:05:23 +0500

    Update version to `0.0.8`.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tonutils",
-    version="0.0.7",
+    version="0.0.8",
     author="nessshon",
     description="Tonutils is a high-level object-oriented library for Python designed to facilitate interactions with the TON blockchain.",
     long_description=long_description,
