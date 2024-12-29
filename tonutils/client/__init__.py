c9e926de16d3ba682886ab7b46fcfeedac86772f
betepookk <79110830+betepookk@users.noreply.github.com>
2024-12-29 14:54:25 +0400

    Update __init__.py

--- tonutils/client/__init__.py
+++ tonutils/client/__init__.py
@@ -3,6 +3,7 @@ from ._base import Client
 from .lite import LiteserverClient
 from .tonapi import TonapiClient
 from .toncenter import ToncenterClient
+from .getblock import GetblockClient
 
 __all__ = [
     "Client",
