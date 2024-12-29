81c0a1251df0dabc181f1ec26924fda42ec977c9
betepookk <79110830+betepookk@users.noreply.github.com>
2024-12-29 14:57:33 +0400

    Update wallet.py

--- tonutils/jetton/contract/stablecoin/wallet.py
+++ tonutils/jetton/contract/stablecoin/wallet.py
@@ -9,6 +9,7 @@ from ....client import (
     LiteserverClient,
     TonapiClient,
     ToncenterClient,
+    GetblockClient
 )
 from ....contract import Contract
 from ....exceptions import UnknownClientError
