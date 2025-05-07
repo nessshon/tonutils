c6c54c843d7d8e3ef50680dc43d4cb58d3ad1309
nessshon <shonness98@gmail.com>
2025-05-07 10:45:46 +0500

    Add `cell_hash` and improve `slice_hash` and `string_hash` docs.

--- tonutils/utils.py
+++ tonutils/utils.py
@@ -184,11 +184,30 @@ def serialize_onchain_dict(data: Dict[str, Any]) -> Cell:
     return dict_cell.serialize()
 
 
+def cell_hash(c: Cell) -> Tuple[bytes, int]:
+    """
+    Calculates the representation hash of the given cell c and returns it as a 256-bit unsigned integer x.
+    This function is handy for signing and verifying signatures of arbitrary entities structured as a tree of cells.
+    """
+    h = c.hash
+    return h, int.from_bytes(h, "big")
+
+
 def slice_hash(s: Slice) -> Tuple[bytes, int]:
-    s_hash = s.to_cell().hash
-    s_int = int.from_bytes(s_hash, "big")
-    return s_hash, s_int
+    """
+    Computes the hash of the given slice s and returns it as a 256-bit unsigned integer x.
+    The result is equivalent to creating a standard cell containing only the data and references from s and then computing its hash using cell_hash.
+    """
+    h = s.to_cell().hash
+    return h, int.from_bytes(h, "big")
 
 
 def string_hash(s: str) -> Tuple[bytes, int]:
-    return slice_hash(begin_cell().store_snake_string(s))
+    """
+    Calculates the SHA-256 hash of the data bits in the given slice s.
+    A cell underflow exception is thrown if the bit length of s is not a multiple of eight.
+    The hash is returned as a 256-bit unsigned integer x.
+    """
+    data = begin_cell().store_snake_string(s).end_cell().get_data_bytes()
+    sha256 = hashlib.sha256(data).digest()
+    return sha256, int.from_bytes(sha256, "big")
