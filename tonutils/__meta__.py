ba67b10a7d1e12b4daf8da5447febbbf5e2c2fcb
ness <shonness98@gmail.com>
2026-07-23 00:12:04 +0500

    Update version to `2.1.1`

--- tonutils/__meta__.py
+++ tonutils/__meta__.py
@@ -3,6 +3,6 @@
 # This source code is licensed under the MIT License found in the
 # LICENSE file in the root directory of this source tree.
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 __author__ = "nessshon"
 __url__ = "https://github.com/nessshon/tonutils"
